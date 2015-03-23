707c13929a82913ff66fee25efd51fecbde554fd
Ethan-zhengyw <zhengyw55@gmail.com>
2015-03-23 23:15:03 +0800

    upload first two step

---
+++ BasicUI_and_Animation/BasicUI_and_Animation/PersonInfo.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace BasicUI_and_Animation
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class PersonInfo : Page
+    {
+        public PersonInfo()
+        {
+            this.InitializeComponent();
+        }
+
+        /**
+         * the Icon button was clicked and this function 
+         * will be called to select another picther to replace the current one
+         */
+        private async void selectIcon(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker imagePicker = new FileOpenPicker();
+            //imagePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;  // 默认起始目录为图片收藏
+            imagePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            imagePicker.ViewMode = PickerViewMode.List;
+
+            // 设置查找的文件类型，均为图片
+            imagePicker.FileTypeFilter.Add(".bmp");
+            imagePicker.FileTypeFilter.Add(".png");
+            imagePicker.FileTypeFilter.Add(".jpg");
+            imagePicker.FileTypeFilter.Add(".jpeg");
+
+            //打开单个图片
+            StorageFile file = await imagePicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var stream = await file.OpenAsync(FileAccessMode.Read);
+                var image = new BitmapImage();
+                image.SetSource(stream);
+                this.Icon.Source = image;
+            }
+        }
+
+        private void viewAnimation(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Animation));
+        }
+        private void backToTestPage(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
---
+++ DataBinding_and_ViewTransition/DataBinding_and_ViewTransition/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace DataBinding_and_ViewTransition
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            List<Book> books = new List<Book>() {
+                new Book("程序员面试宝典", "欧力奇", "2011", "http://202.112.150.126/index.php?client=libcode&isbn=978-7-121-12647-5/cover"),
+                new Book("SQL应用开发参考手册", "郭鑫", "2014", "http://202.112.150.126/index.php?client=libcode&isbn=978-7-111-44169-4/cover"),
+                new Book(".NET最佳实践", "里奇", "2014", "http://202.112.150.126/index.php?client=libcode&isbn=978-7-111-44912-6/cover"),
+                new Book("Windows 8应用开发实战 : 使用JavaScript", "塞尔斯", "2014", "http://202.112.150.126/index.php?client=libcode&isbn=978-7-111-44745-0/cover"),
+                new Book("高效能程序员的修炼", "阿特伍德", "2013", "http://202.112.150.126/index.php?client=libcode&isbn=978-7-115-31898-5/cover")
+            };
+
+            bookFliper.ItemsSource = books;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GridView));
+        }
+    }
+}
